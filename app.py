f384ff90e4ed825b6693062e578a6a82e83d7147
Mayu Hanashiro <maha@example.com>
2022-02-28 21:36:11 +0900

    app.py修正

--- app.py
+++ app.py
@@ -9,27 +9,45 @@ def test():
     return  render_template("base.html")    
 
 
+@app.route("/top")
+def top():
+    return render_template("top.html")
+
+
 @app.route("/login")
 def login():
     return render_template("login.html")
 
 
-@app.route("/list")
-def task_list():
-    conn = sqlite3.connect("fkask_test2.db")
-    c = conn.cursor()
-    c.execute("SELECT * FROM tasks;")
-    task_list = []
-    
-    for row in c.fetchall():
-        task_list.append({"id":row[0],"task":row[1]})
-    
-    c.close()
-    return render_template("task_list.html",task_list=task_list)
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
-@app.route("/add")
-def add_get():
-    return render_template("add.html")
+
+@app.route("/second_top")
+def second_top():
+    return render_template("second_top.html")
+
+
+@app.route("/image_upload")
+def image_upload():
+    return render_template("image_upload.html")
+
+
+@app.route("/main_upload")
+def main_upload():
+    return render_template("main_upload.html")
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/release")
+def release():
+    return render_template("release.html")
+
     
 if __name__ == '__main__':
     app.run(debug =True)
